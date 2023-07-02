34b038d0a00e69cd0dad207d9de8b7f0ee49de35
Jitendra A <2908547+adhocore@users.noreply.github.com>
2023-07-02 11:48:17 +0700

    feat: is anagram

---
+++ isanagram.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s, t):
+        if len(s) != len(t): return False
+        cc = {}
+        for c in s: cc[c] = cc[c]+1 if c in cc else 1
+        for c in t:
+            if c not in cc: return False
+            cc[c] -= 1
+            if cc[c] == 0: del(cc[c])
+        return True
+
+    # one line solution:
+    def isAnagram2(self, s, t):
+        return len(s) == len(t) and sorted(s) == sorted(t)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.isAnagram("anagram", "nagaram"))
+    # => True
