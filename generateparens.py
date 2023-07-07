ff0aa5d60dcb4543d512cd2df602d48b3de39e64
Jitendra A <2908547+adhocore@users.noreply.github.com>
2023-07-07 12:38:53 +0700

    style: lint

--- generateparens.py
+++ generateparens.py
@@ -7,14 +7,15 @@ def generate_parenthesis(num: int) -> list[str]:
     Space Complexity: O(1)
     Time Complexity:  O(n)
     """
+
     def generate(open_count: int, close_count: int, gen: str):
         if open_count == num and close_count == num:
             ans.append(gen)
             return
         if open_count < num:
-            generate(open_count + 1, close_count, gen + '(')
+            generate(open_count + 1, close_count, gen + "(")
         if open_count > close_count:
-            generate(open_count, close_count + 1, gen + ')')
+            generate(open_count, close_count + 1, gen + ")")
 
     ans = []
     generate(0, 0, "")
